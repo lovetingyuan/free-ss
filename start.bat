c259891710ec857b56b742cd00dcf368faa78ff5
huangbaitu <baitu.huang@zuolin.com>
2016-04-20 20:25:05 +0800

    更改目录结构

--- start.bat
+++ start.bat
@@ -1,6 +1,5 @@
 @title ¿ÆÑ§ÉÏÍø %DATE:~0,4%-%DATE:~5,2%-%DATE:~8,2% %TIME:~0,2%:%TIME:~3,2%:%TIME:~6,2%
 @mode con cols=50 lines=15 & color 02
-start chrome http://www.google.com/
 node index
 echo.
 
