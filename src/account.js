dde2c7c1b8d958c79f213de949c8de40d508bd7c
lovetingyuan <1932294867@qq.com>
2016-12-07 14:22:29 +0800

    更新ss客户端

--- src/account.js
+++ src/account.js
@@ -3,6 +3,9 @@ function setSSAccount(accountUrl) {
   const { configPath} = require('./enum');
   let grabUrl = require('./net').grabUrl;
   return grabUrl(accountUrl).then(data => {
+    var file = require('./file');
+    var config = file.Json(configPath).read();
+    let sampleServer = config.configs[0];
     let configs = [];
     let h4s = data.match(/<h4>(.*)?<\/h4>/g)
       .slice(0, 18)
@@ -10,19 +13,16 @@ function setSSAccount(accountUrl) {
       .map(v => v.replace(/<h4>[\s\S]+?:([\s\S]*)?<\/h4>/, '$1'));
     for (let i = 0; i < h4s.length; i += 4) {
       if (h4s[i] && h4s[i + 1] && h4s[i + 2] && h4s[i + 3]) {
-        configs.push({
+        configs.push(Object.assign({}, sampleServer, {
           "server": h4s[i],
           "server_port": +h4s[i + 1],
           "password": h4s[i + 2],
           "method": h4s[i + 3],
-          "remarks": "ishadowsocks",
-          "auth": false
-        });
+        }));
       }
     }
     if (!configs.length) {
       throw Error('no available ss account...');
-      return;
     }
     return configs;
   }).then(function(configs) {
