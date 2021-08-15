5a7d6da63f1d4ecc703cde7f62a0e1f50a34607d
tingyuan <hbtwhucsis@gmail.com>
2021-08-15 14:22:43 +0800

    chore

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ const clipboardy = require('clipboardy');
 const childProcess = require('child_process')
 const path = require('path')
 const fs = require('fs')
+const ssDir = path.resolve(__dirname, 'shadowsocks')
 
 const atob = (a) => {
   return Buffer.from(a, 'base64').toString('utf-8');
+}
+const request = url => {
+  return fetch(url).then(r => {
+    if (r.ok) return r.text()
+    return Promise.reject(`fetch error: ${r.status}, ${r.statusText}`)
+  })
 }
 
 function parseSSUrl (url) {
@@ -25,11 +32,13 @@ function parseSSUrl (url) {
 }
 
 function updateAccounts(accounts) {
-  const configFile = path.resolve(__dirname, 'shadowsocks', 'gui-config.json')
+  const configFile = path.resolve(ssDir, 'gui-config.json')
   if (!fs.existsSync(configFile)) {
-    console.log('no shadowsocks config file, please run ss first.')
+    console.log('no shadowsocks config file, please run ss client first.')
     restartClient()
-    process.exit(0)
+    setTimeout(() => {
+      process.exit(0) 
+    });
   }
   const config = require(configFile)
   config.configs = accounts;
@@ -40,7 +49,7 @@ function updateAccounts(accounts) {
 
 function restartClient() {
   const ssbinName = 'Shadowsocks.exe'
-  const ssbinPath = path.resolve(__dirname, 'shadowsocks', ssbinName)
+  const ssbinPath = path.resolve(ssDir, ssbinName)
   const ret = childProcess.execSync(`tasklist /FI "IMAGENAME eq ${ssbinName}" /FO csv /NH`).toString('utf8')
   if (ret.includes(ssbinName)) {
     console.log('closing current client...')
@@ -52,8 +61,10 @@ function restartClient() {
         process.kill(_pid, 'SIGINT')
       }
     }
+    console.log('restarting ss client...')
+  } else {
+    console.log('starting ss client...')
   }
-  console.log('starting ss client...')
   return childProcess.spawn(ssbinPath, {
     detached: true
   })
@@ -61,32 +72,33 @@ function restartClient() {
 
 function fetchAccounts() {
   console.log('start fetching accounts...')
-  return fetch('https://bulink.me/sub/mn2fa/ss').then(r => r.text()).catch(() => {
-    return fetch('https://raw.fastgit.org/freefq/free/master/v2').then(r => r.text())
+  return request('https://raw.fastgit.org/freefq/free/master/v2').catch(() => {
+    return request('https://bulink.me/sub/mn2fa/ss')
   }).then(r => {
     const str = atob(r)
     const ssAccounts = str.split('\n').filter(a => a.startsWith('ss://'))
     if (!ssAccounts.length) {
-      return console.log('No avaliable accounts.')
+      return Promise.reject(new Error('no available accounts.'))
     }
     console.log(`fetched ${ssAccounts.length} accounts.`)
     clipboardy.writeSync(ssAccounts.join('\n'));
     return ssAccounts.map(parseSSUrl)
-  }).catch(e => {
-    console.warn('failed to fetch accounts, ' + e.message)
   })
 }
 
 function main () {
   return fetchAccounts().then(accounts => {
-    return updateAccounts(accounts)
-  }).then(() => {
-    return restartClient()
-  }).then(() => {
-    console.log('done!')
-    process.exit(0)
+    if (accounts) {
+      updateAccounts(accounts)
+      restartClient()
+      console.log('done, quit after 3 seconds.')
+      setTimeout(() => {
+        process.exit(0)
+      }, 3000)
+    }
   }).catch(err => {
-    console.error(err)
+    const msg = err instanceof Error ? err.message : (err ? err.toString() : 'unknown reason.')
+    console.error('failed, ' + msg)
   })
 }
 
